f92f4d75548cd40374aee5ab1a9c6749e12662da
yuvipanda <yuvipanda@gmail.com>
2017-05-20 17:52:38 -0700

    Lots of fixes
    - Use the Simple HTML page to get list of all packages. The
      XML RPC API seems to be lying?
    - Use one ServerProxy per thread. The aren't thread safe, but
      it is ok to have one per thread in the pool

--- dump.py
+++ dump.py
@@ -2,11 +2,21 @@ import os
 import xmlrpc.client
 import time
 import json
+import lxml.html
+import threading
 
+from urllib.request import urlopen
 from concurrent.futures import ThreadPoolExecutor
 
 
 def dump_package(package):
+    tls = threading.local()
+    # Reuse the ServerProxy objects!
+    if hasattr(tls, 'client'):
+        client = tls.client
+    else:
+        client = xmlrpc.client.ServerProxy('https://pypi.python.org/pypi')
+        tls.client = client
     filepath = '%s.json' % package
     if os.path.exists(filepath):
         print('skipping ', package)
@@ -22,13 +32,11 @@ def dump_package(package):
 
     print('Done {} in {}s'.format(package, time.time() - start_time))
 
-client = xmlrpc.client.ServerProxy('https://pypi.python.org/pypi')
 
-packages = client.list_packages()
-
+simple_doc = lxml.html.parse(urlopen('https://pypi.python.org/simple/'))
 pool = ThreadPoolExecutor()
 
-for package in packages:
+for _, _, package, _ in simple_doc.getroot().iterlinks():
     pool.submit(dump_package, package)
 
 
