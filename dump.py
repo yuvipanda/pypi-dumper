caf5b67af2412177d38680f58d3fd41aa7861535
yuvipanda <yuvipanda@gmail.com>
2017-05-20 17:30:26 -0700

    Print time taken for each package

--- dump.py
+++ dump.py
@@ -1,5 +1,6 @@
 import os
 import xmlrpc.client
+import time
 import json
 
 from concurrent.futures import ThreadPoolExecutor
@@ -11,6 +12,7 @@ def dump_package(package):
         print('skipping ', package)
         return
 
+    start_time = time.time()
     versions = [client.release_data(package, version) for version in client.package_releases(package, True)]
 
     with open(filepath, 'w') as f:
@@ -18,7 +20,7 @@ def dump_package(package):
             f.write(json.dumps(v) + '\n')
 
 
-    print('Done ', package)
+    print('Done {} in {}s'.format(package, time.time() - start_time))
 
 client = xmlrpc.client.ServerProxy('https://pypi.python.org/pypi')
 
@@ -27,7 +29,7 @@ packages = client.list_packages()
 pool = ThreadPoolExecutor()
 
 for package in packages:
-    pool.submit(lambda: dump_package(package))
+    pool.submit(dump_package, package)
 
 
 pool.shutdown()
